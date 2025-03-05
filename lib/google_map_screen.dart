d1712a9e567986dc373fa37b83c6bdae188ca9af
johir <mdjohiruli826@gmail.com>
2025-03-05 17:38:00 +0600

    added

--- lib/google_map_screen.dart
+++ lib/google_map_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:google_places_flutter/google_places_flutter.dart';
 
 class GoogleMapScreen extends StatefulWidget {
   const GoogleMapScreen({super.key});
@@ -9,59 +12,24 @@ class GoogleMapScreen extends StatefulWidget {
 }
 
 class _GoogleMapScreenState extends State<GoogleMapScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getUserLocation();
+  }
+
+  final TextEditingController _searchController = TextEditingController();
+  final String _googleKey = "AIzaSyC-Uu7Kic0MsR77V1SB_SHhfzHLxCmEZ24";
   late final GoogleMapController _mapController;
   int _selectedIndex = 0;
-
-  final Set<Marker> _markers = <Marker>{
-    Marker(
-      markerId: MarkerId('my-home'),
-      position: LatLng(23.819481190679497, 90.40947441011667),
-      infoWindow: InfoWindow(title: 'My Home', onTap: () {}),
-      onTap: () {},
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-      draggable: true,
-      onDragStart: (LatLng startLatLng) {
-        print('Start LatLng $startLatLng');
-      },
-      onDragEnd: (LatLng endLatLng) {
-        print('End LatLng $endLatLng');
-      },
-    ),
-    Marker(
-      markerId: MarkerId('my-office'),
-      position: LatLng(23.821477296806954, 90.40332645177841),
-      infoWindow: InfoWindow(title: 'My Office', onTap: () {}),
-      onTap: () {},
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-      draggable: true,
-      onDragStart: (LatLng startLatLng) {
-        print('Start LatLng $startLatLng');
-      },
-      onDragEnd: (LatLng endLatLng) {
-        print('End LatLng $endLatLng');
-      },
-    ),
-    Marker(
-      markerId: MarkerId('my-club'),
-      position: LatLng(23.81758044461688, 90.40547255426645),
-      infoWindow: InfoWindow(title: 'My Club', onTap: () {}),
-      onTap: () {},
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-      draggable: true,
-      onDragStart: (LatLng startLatLng) {
-        print('Start LatLng $startLatLng');
-      },
-      onDragEnd: (LatLng endLatLng) {
-        print('End LatLng $endLatLng');
-      },
-    ),
-  };
+  LatLng _currentLocation = LatLng(0.0, 0.0);
+  final Set<Marker> _markers = <Marker>{};
 
   final Set<Polyline> _polyLine = <Polyline>{
     Polyline(
       polylineId: PolylineId('my-home'),
       points: [
-        LatLng(23.819481190679497, 90.40947441011667),
         LatLng(23.821477296806954, 90.40332645177841),
         LatLng(23.81758044461688, 90.40547255426645),
       ],
@@ -72,7 +40,6 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
       jointType: JointType.round,
     ),
   };
-
   final Set<Circle> _circle = <Circle>{
     Circle(
       circleId: CircleId('most-effected'),
@@ -115,92 +82,81 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
             compassEnabled: true,
             mapToolbarEnabled: true,
             initialCameraPosition: CameraPosition(
-              target: LatLng(23.819402364100583, 90.40612634271383),
+              target: _currentLocation,
               zoom: 16,
             ),
             onMapCreated: (GoogleMapController controller) {
               _mapController = controller;
             },
-
-            onTap: (LatLng latLng) {
-              print('Tapped on $latLng');
-            },
-            onLongPress: (LatLng latLng) {
-              print('Long Pressed at $latLng');
-            },
             markers: _markers,
+            onTap: _addMarker,
             polylines: _polyLine,
             circles: _circle,
             polygons: _polygon,
           ),
-          Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 32),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 60,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(48),
-                    ),
-                    color: Colors.white,
-                    child: Row(
-                       mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        SizedBox(width: 3,),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [Icon(Icons.location_on,size: 32,color: Colors.teal,)],
-                        ),
-                        SizedBox(width: 24),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [Text('Search here')],
-                        ),
-                        SizedBox(width: 90),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [Icon(Icons.mic,size: 28,)],
-                        ),
-                        SizedBox(width: 8),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [CircleAvatar()],
-                        ),
-                        SizedBox(width: 3,),
-                      ],
-                    ),
-                  ),
-                ),
+          Positioned(
+            top: 40,
+            left: 8,
+            right: 8,
+            child: Card(
+              color: Colors.white,
+              shadowColor: Colors.white,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(24)),
               ),
-            ],
-          ),
-
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+              child: Column(
                 children: [
-                  FloatingActionButton(
-                    onPressed: () {
-                      _currentMyLocation();
-                    },
-                    child: Icon(Icons.gps_not_fixed, color: Colors.black),
-                    backgroundColor: Colors.white,
+                  GooglePlaceAutoCompleteTextField(
+                    boxDecoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    textEditingController: _searchController,
+                    googleAPIKey: _googleKey,
+                    inputDecoration: InputDecoration(
+                      border: InputBorder.none,
+
+                      prefixIcon: Icon(Icons.location_on, size: 32),
+                      prefixIconColor: Colors.teal,
+                      suffixIcon: Icon(Icons.mic, size: 30),
+                      hintText: 'Search here',
+                      hintStyle: TextStyle(
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 24,
+                      ),
+                    ),
                   ),
-                  SizedBox(width: 16),
-                  FloatingActionButton(
-                    onPressed: () {},
-                    child: Icon(Icons.turn_right_outlined, color: Colors.white),
-                    backgroundColor: Colors.teal,
-                  ),
-                  SizedBox(width: 16),
                 ],
               ),
-              SizedBox(height: 24),
-            ],
+            ),
+          ),
+
+          Positioned(
+            bottom: 15,
+            right: 16,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    _getUserLocation();
+                  },
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(26),
+                  ),
+                  child: Icon(Icons.my_location, color: Colors.blue),
+                ),
+                SizedBox(height: 20),
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Colors.teal,
+                  child: Icon(Icons.turn_right_outlined, color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ],
       ),
@@ -230,14 +186,31 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
     );
   }
 
-  void _currentMyLocation() {
-    _mapController.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target: LatLng(23.819402364100583, 90.40612634271383),
-          zoom: 16,
-        ),
-      ),
+  Future<void> _getUserLocation() async {
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      setState(() {
+        _currentLocation = LatLng(position.latitude, position.longitude);
+      });
+      _mapController.animateCamera(
+        CameraUpdate.newLatLngZoom(_currentLocation, 16),
+      );
+    }
+  }
+
+  void _addMarker(LatLng positionMarker) {
+    final Marker marker = Marker(
+      markerId: MarkerId('marker_1'),
+      position: positionMarker,
+      infoWindow: InfoWindow(title: 'This is InfoWindow', onTap: () {}),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+      draggable: true,
     );
+    setState(() {
+      _markers.add(marker);
+    });
   }
 }
